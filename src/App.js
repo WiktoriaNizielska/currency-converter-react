1b110eb9f0e9e2f15b4189816687529212445c7f
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2023-08-05 12:17:34 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<form className=" form">
+        <fieldset className="form__fieldset">
+            <legend className="form__legend">Przelicznik walut</legend>
+            <p>
+                <label>
+                    <span className="form__labelText">Waluta:</span>
+                    <select name="currency" className=" form__field">
+                        <option value="USD">USD - Dolar amerykański</option>
+                        <option value="EUR">EUR - Euro</option>
+                        <option value="SEK">SEK - Korona szwedzka</option>
+                        <option value="CHF">CHF - Frank szwajcarski</option>
+                    </select>
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className="form__labelText">Kwota w złotówkach:</span>
+                    <input className=" form__field" type="number" name="amount" min="0.01" step="0.01"/>
+                </label>
+            </p>
+        </fieldset>
+        <p> <button className="form__button">Przelicz</button> </p>
+    </form>
+    <p className="form__result">Wynik: </p>
     </div>
   );
 }
