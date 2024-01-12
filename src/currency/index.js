049082b445e7565fa7d47573fdc1fc2151ac3dd9
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-01-12 18:01:52 +0100

    currencies added

---
+++ src/currency/index.js
@@ -0,0 +1,8 @@
+const currencies = [
+    { id: 1, currency: "Dolar amerykański", symbol: "USD", rate: 3.97 }
+    { id: 2, currency: "Euro", symbol: "EUR", rate: 4.35 }
+    { id: 3, currency: "Korona szwedzka", symbol: "SEK", rate: 0.38 }
+    { id: 4, currency: "Frank szwajcarski", symbol: "CHF", rate: 4.65 }
+]
+
+export default currencies;
