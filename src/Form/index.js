6af82e58af17a0098c7572f2b8fd0d460e8c3db8
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-01-16 10:54:40 +0100

    currency select added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,28 +1,59 @@
 import "./style.css";
+import { useState } from "react";
+import currencies from "../currencies";
 
-const Form = () => (
-    <form className=" form">
-        <fieldset className="form__fieldset">
-            <legend className="form__legend">Przelicznik walut</legend>
-            <p>
-                <label>
-                    <span className="form__labelText">Waluta:</span>
-                    <select name="currency" className=" form__field">
-                        <option value="USD">USD - Dolar amerykański</option>
-                        <option value="EUR">EUR - Euro</option>
-                        <option value="SEK">SEK - Korona szwedzka</option>
-                        <option value="CHF">CHF - Frank szwajcarski</option>
-                    </select>
-                </label>
-            </p>
-            <p>
-                <label>
-                    <span className="form__labelText">Kwota w złotówkach:</span>
-                    <input className=" form__field" type="number" name="amount" min="0.01" step="0.01" />
-                </label>
-            </p>
-        </fieldset>
+const Form = () => {
+  const [currency, setCurrency] = useState("");
+  const onSelectChange = ({ target }) => setCurrency(target.value);
+
+  const onFormsubmit = (event) => {
+    event.preventDefault();
+    console.log(`Wysłano: ${currency}`);
+  };
+
+  return (
+    <form className=" form" onSubmit={onFormsubmit}>
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">
+          Przelicznik walut
+        </legend>
+        <p>
+          <label>
+            <span className="form__labelText">
+              Waluta:
+            </span>
+            <select
+              value={currency}
+              onChange={onSelectChange}
+              className=" form__field"
+            >
+              {currencies.map((currency => (
+                <option
+                  value={currencies.currency}
+                  key={currency.short}
+                >
+                  {currency.symbol} - {currency.currency}
+                </option>
+              )))}
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            <span className="form__labelText">
+              Kwota w złotówkach:
+            </span>
+            <input
+              className=" form__field"
+              type="number"
+              name="amount"
+              min="0.01"
+              step="0.01" />
+          </label>
+        </p>
+      </fieldset>
     </form>
-);
+  );
+}
 
 export default Form;
