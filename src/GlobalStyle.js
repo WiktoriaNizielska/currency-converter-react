94a4f9a72490202cd9ab528e0596787e8e1c7d80
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-05-14 20:46:57 +0200

    Global Style added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./background.jpg"
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 10px auto;
+    max-width: 650px;
+    padding: 20px;
+    font-family: 'Montserrat', sans-serif;
+    color: ${({ theme }) => theme.color.capeCod};
+    background-image: url("${background}");
+  }
+`;
