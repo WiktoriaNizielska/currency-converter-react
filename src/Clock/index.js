5ffb5271677a1908bd30e687963bba1c36330658
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-05-14 10:57:13 +0200

    date formatting changed

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,26 +1,23 @@
 import { ClockContainer } from "./styled";
 import { useCurrentDate } from "./useCurrentDate";
 
+const formatDate = (date) => date.toLocaleString(undefined,
+  {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+  });
+
 const Clock = () => {
   const date = useCurrentDate();
 
   return (
     <ClockContainer>
       Dzisiaj jest&nbsp;
-      {date.toLocaleString(undefined,
-        {
-          weekday: "long",
-          day: "numeric",
-          month: "long",
-        }
-      )},&nbsp;
-      {date.toLocaleString(undefined,
-        {
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric"
-        }
-      )}
+      {formatDate(date)}
     </ClockContainer>
   )
 };
