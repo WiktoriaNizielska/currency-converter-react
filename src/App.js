46a07dc393165fca34654440fe53f84a778b7d51
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-06-13 11:06:02 +0200

    ratesData created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import { useEffect, useState } from "react";
 import Form from "./Form";
 import Result from "./Result";
-import currencies from "./currencies";
 import Clock from "./Clock";
 import axios from "axios";
 
 function App() {
-  const [ratesData, setRatesData] = useState({});
+  const [ratesData, setRatesData] = useState({
+    data: null,
+    loading: false,
+    error: null,
+    date: null,
+  });
+
   useEffect(() => {
     const fetchRates = async () => {
       try {
+        setRatesData((previousData) => ({
+          ...previousData,
+          loading: true,
+        }));
+
         const response = await axios.get("currencies.json");
         console.log(response.data);
+
+        setRatesData({
+          data: response.data.data,
+          loading: false,
+          error: null,
+          date: response.data.meta.last_updated_at,
+        });
       } catch (error) {
         console.error("coś nie poszło", error);
       }
@@ -19,14 +36,10 @@ function App() {
     fetchRates();
   }, []);
 
-
-
   const [result, setResult] = useState(null);
 
   const calculateResult = (amount, currency) => {
-    const rate = currencies
-      .find(({ symbol }) => symbol === currency)
-      .rate;
+    const rate = ratesData.data[currency].value;
 
     setResult({
       sourceAmount: +amount,
@@ -41,6 +54,7 @@ function App() {
         Clock={Clock}
         calculateResult={calculateResult}
         result={result}
+        ratesData={ratesData.data}
       />
       <Result
         result={result}
